24c9f741ea9ea01d862fd5c1fe276b07365d13c1
Ian Huntington <ihuntington85@gmail.com>
2013-04-30 18:00:18 +0100

    Corrected routes

--- mongo_test/server.js
+++ mongo_test/server.js
@@ -6,12 +6,13 @@ var app = express();
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
+    app.use(express.methodOverride());
 });
- 
-app.get('geoLoc', geoLoc.findAll);
-app.get('geoLoc/:id', geoLoc.findById);
-app.post('geoLoc', geoLoc.addgeoLoc);
-app.put('geoLoc/:id', geoLoc.updategeoLoc);
-app.delete('geoLoc/:id', geoLoc.deletegeLoc);
- 
+
+app.get('/geoLoc', geoLoc.findAll);
+app.get('/geoLoc/:id', geoLoc.findById);
+app.post('/geoLoc', geoLoc.addgeoDb);
+app.put('/geoLoc/:id', geoLoc.updategeoDb);
+app.delete('/geoLoc/:id', geoLoc.deletegeoDb);
+
 app.listen(3000);
